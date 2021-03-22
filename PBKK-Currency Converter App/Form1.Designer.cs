841d60e9dc8cf02b87c241507d10ea62c9dd8a66
Seijaku <abdrohman0100@gmail.com>
2021-03-22 23:40:09 +0700

    First Commit

---
+++ PBKK-Currency Converter App/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace PBKK_Currency_Converter_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.labelFrom = new System.Windows.Forms.Label();
+            this.comboBoxFrom = new System.Windows.Forms.ComboBox();
+            this.numInput = new System.Windows.Forms.NumericUpDown();
+            this.labelTo = new System.Windows.Forms.Label();
+            this.comboBoxTo = new System.Windows.Forms.ComboBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.Location = new System.Drawing.Point(74, 21);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(405, 46);
+            this.labelTitle.TabIndex = 0;
+            this.labelTitle.Text = "Currency Convertion";
+            // 
+            // labelFrom
+            // 
+            this.labelFrom.AutoSize = true;
+            this.labelFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFrom.Location = new System.Drawing.Point(43, 83);
+            this.labelFrom.Name = "labelFrom";
+            this.labelFrom.Size = new System.Drawing.Size(63, 26);
+            this.labelFrom.TabIndex = 1;
+            this.labelFrom.Text = "From";
+            // 
+            // comboBoxFrom
+            // 
+            this.comboBoxFrom.FormattingEnabled = true;
+            this.comboBoxFrom.Location = new System.Drawing.Point(48, 125);
+            this.comboBoxFrom.Name = "comboBoxFrom";
+            this.comboBoxFrom.Size = new System.Drawing.Size(71, 21);
+            this.comboBoxFrom.TabIndex = 5;
+            this.comboBoxFrom.SelectedIndexChanged += new System.EventHandler(this.comboBoxFrom_SelectedIndexChanged);
+            // 
+            // numInput
+            // 
+            this.numInput.Location = new System.Drawing.Point(125, 125);
+            this.numInput.Name = "numInput";
+            this.numInput.Size = new System.Drawing.Size(120, 20);
+            this.numInput.TabIndex = 3;
+            this.numInput.ValueChanged += new System.EventHandler(this.numInput_ValueChanged);
+            // 
+            // labelTo
+            // 
+            this.labelTo.AutoSize = true;
+            this.labelTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTo.Location = new System.Drawing.Point(299, 83);
+            this.labelTo.Name = "labelTo";
+            this.labelTo.Size = new System.Drawing.Size(36, 26);
+            this.labelTo.TabIndex = 4;
+            this.labelTo.Text = "To";
+            // 
+            // comboBoxTo
+            // 
+            this.comboBoxTo.FormattingEnabled = true;
+            this.comboBoxTo.Location = new System.Drawing.Point(304, 125);
+            this.comboBoxTo.Name = "comboBoxTo";
+            this.comboBoxTo.Size = new System.Drawing.Size(70, 21);
+            this.comboBoxTo.TabIndex = 5;
+            this.comboBoxTo.SelectedIndexChanged += new System.EventHandler(this.comboBoxTo_SelectedIndexChanged);
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(379, 126);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(100, 20);
+            this.textBoxOutput.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 249);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.comboBoxTo);
+            this.Controls.Add(this.labelTo);
+            this.Controls.Add(this.numInput);
+            this.Controls.Add(this.comboBoxFrom);
+            this.Controls.Add(this.labelFrom);
+            this.Controls.Add(this.labelTitle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numInput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Label labelFrom;
+        private System.Windows.Forms.ComboBox comboBoxFrom;
+        private System.Windows.Forms.NumericUpDown numInput;
+        private System.Windows.Forms.Label labelTo;
+        private System.Windows.Forms.ComboBox comboBoxTo;
+        private System.Windows.Forms.TextBox textBoxOutput;
+       
+    }
+}
+
